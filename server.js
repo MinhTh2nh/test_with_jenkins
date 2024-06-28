57fb105abfbc66366ac7e87a5416b73134751953
LAPTOP-JJ518A0L\minht <minhthanh5346@gmail.com>
2024-06-28 22:32:45 +0700

    adding

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/router-test-1', (req, res) => {
   res.send('Hello World 1');
 });
 
+app.get('/router-test-2', (req, res) => {
+    res.send('Hello World 2');
+  });
+
 app.listen(port, hostname, () => {
   console.log(`Your app is running on http://${hostname}:${port}/`);
 });
