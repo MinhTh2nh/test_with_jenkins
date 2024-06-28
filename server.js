e96073dce735af2be69e1b14de624b78fa357913
LAPTOP-JJ518A0L\minht <minhthanh5346@gmail.com>
2024-06-28 22:27:53 +0700

    first

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World');
+});
+
+app.get('/router-test-1', (req, res) => {
+  res.send('Hello World 1');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Your app is running on http://${hostname}:${port}/`);
+});
